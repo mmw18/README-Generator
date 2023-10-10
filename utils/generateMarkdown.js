1da131be371b5b51fe845ae80bd25cf5b027af18
Megan Wright <meganmwright2018@gmail.com>
2023-10-09 21:09:28 -0500

    adding commenting to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
+/* Function that will make a badge appear with license information if license is chosen.
+Function specifies that if no license is selected, there will not be a badge produced */
 function renderLicenseBadge(license) {
   if (license === "N/A") {
     return "";
@@ -7,8 +8,9 @@ function renderLicenseBadge(license) {
     return `![alt text](https://img.shields.io/badge/${license}-License-blue)`
  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+/* Function that will create a linked item for the license on the 
+table of contents. If no license is chosen, this extra line/link
+will not be added to the table of contents*/
 function renderLicenseLink(license) {
   if (license === "N/A") {
     return "";
@@ -16,8 +18,10 @@ function renderLicenseLink(license) {
   return '- [License](#license)'
  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+/* Function that will create the actual license section if one is chosen, within
+the README. The section title will be 'License', and a statement to reflect
+which license has been chosen will appear. If no license is selected, this
+section will not appear on the README */
 function renderLicenseSection(license) { 
   if (license === "N/A") {
     return ""
@@ -26,7 +30,8 @@ function renderLicenseSection(license) {
   This project is licensed under the ${license} License`
 }
 
-// TODO: Create a function to generate markdown for README
+/* Function that will create the markdown file with the contents
+of the answers to prompted questions in the terminal */
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
@@ -58,5 +63,6 @@ ${renderLicenseSection(data.license)}
 
 `;
 }
-
+// Exporting the generateMarkdown function so that it may be used in our index.js.
+// This will allow the question answers to be used in the gM function
 module.exports = generateMarkdown;
